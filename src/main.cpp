5b902837bbe487435fc99302fd021c48eb9cd171
C9H13NO3-dev <57861146+C9H13NO3-dev@users.noreply.github.com>
2024-04-15 16:43:29 +0200

    Added OTA updating
    Main goal is to be able to push updates to the scale via wifi. No more fiddling with a USB cable and destroying the flimsy cabling ;)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoOTA.h>
 
 #include "display.hpp"
 #include "scale.hpp"
@@ -9,34 +10,67 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 const char* ssid     = "ThisIsNotTheWifiYoureLookingFor"; // Change this to your WiFi SSID
-const char* password = "244466666"; // Change this to your WiFi password
+const char* password = "PASSWORD"; // Change this to your WiFi password
 long lastReconnectAttempt = 0;
 
 void setup() {
   Serial.begin(115200);
-  while(!Serial){delay(100);}
+  while(!Serial){ delay(100); }
 
   setupDisplay();
   setupScale();
 
   Serial.println();
   Serial.println("******************************************************");
-  // Serial.print("Connecting to ");
-  // Serial.println(ssid);
 
-  // WiFi.begin(ssid, password);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(500);
-  //   Serial.print(".");
-  // }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 
-  // Serial.println("");
-  // Serial.println("WiFi connected");
-  // Serial.println("IP address: ");
-  // Serial.println(WiFi.localIP());
-  // lastReconnectAttempt = 0;
-  // client.setServer("192.168.1.201", 1883);
+  // Initialize OTA Handlers
+  ArduinoOTA.setHostname("coffee-scale-ota"); // Set the hostname for the OTA device
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready for OTA updates");
+
+  lastReconnectAttempt = 0;
+  client.setServer("192.168.1.201", 1883);
 }
 
 boolean reconnect() {
@@ -48,17 +82,19 @@ boolean reconnect() {
 }
 
 void loop() {
-  // if (!client.connected()) {
-  //   long now = millis();
-  //   if (now - lastReconnectAttempt > 5000) {
-  //     lastReconnectAttempt = now;
-  //     // Attempt to reconnect
-  //     if (reconnect()) {
-  //       lastReconnectAttempt = 0;
-  //     }
-  //   }
-  // } else {
-  //   client.loop();
-  // }
+  if (!client.connected()) {
+    long now = millis();
+    if (now - lastReconnectAttempt > 5000) {
+      lastReconnectAttempt = now;
+      // Attempt to reconnect
+      if (reconnect()) {
+        lastReconnectAttempt = 0;
+      }
+    }
+  } else {
+    client.loop();
+  }
+  
+  ArduinoOTA.handle();  // Handle OTA updates
   delay(1000);
 }
